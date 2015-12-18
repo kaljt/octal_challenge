b4aa5fad69cb6d09e1d4567e5410c777e10de760
Jerry Khalil <email.jerry@yahoo.com>
2015-12-18 15:51:21 -0600

    Octal timed challenge

---
+++ octal.rb
@@ -0,0 +1,33 @@
+# 233
+# 2*8^2 + 3*8^1 + 3*8^0 octal
+# take octal string convert to decimal
+BASE = 8
+DIGITS = %w(0 1 2 3 4 5 6 7)
+
+class Octal
+
+  def initialize(value)
+    @octal = value.split('').reverse
+    @decimal = 0
+  end
+
+  def to_decimal
+      if invalid?
+        @decimal = 0
+      else
+        @octal.each_with_index { |value, index| @decimal += (value.to_i * (BASE**index)) }
+        @decimal 
+     end
+  end
+
+  def invalid?
+    status = true
+    @octal.each do |value|
+      if status = !DIGITS.include?(value)
+         return status
+      end
+      end
+      status   
+  end
+
+end
